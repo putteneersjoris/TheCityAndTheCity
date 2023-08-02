02e1b2c949210ef4d65407108205f2e8cb5eba5d
putteneersjoris <putteneersjoris@gmail.com>
2023-08-02 22:40:42 +0200

    new update problem hopefully solved

---
+++ src/data.js
@@ -0,0 +1,42 @@
+var groups = {
+    "theCityAndtheCity": {
+        "dummy_name": [
+            "./extra/dummy_week.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201431.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201557.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201724.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201852.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202017.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202142.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202307.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202434.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202601.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202727.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202854.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731203022.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731203148.gif_preProcessed_long_.png",
+            "./extra/foo_dummy_week.png",
+            "./extra/ref_dummy_week.png"
+        ]
+    },
+    "dummy_group": {
+        "dummy_name": [
+            "./extra/dummy_week.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201431.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201557.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201724.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731201852.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202017.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202142.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202307.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202434.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202601.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202727.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731202854.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731203022.gif_preProcessed_long_.png",
+            "content/dummy_group/dummy_name/dummy_week/processed/recording_20230731203148.gif_preProcessed_long_.png",
+            "./extra/foo_dummy_week.png",
+            "./extra/ref_dummy_week.png"
+        ]
+    }
+}
